0b3ea52103191253d8c6530cc958cb0612f44a9b
Mayur <mayurtyagi@unolo.com>
2025-04-20 10:28:57 +0530

    Initial Commit

---
+++ FocusProductivityApp/FocusProductivityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusProductivityAppApp.swift
+//  FocusProductivityApp
+//
+//  Created by Mayur Kant Tyagi on 20/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusProductivityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
